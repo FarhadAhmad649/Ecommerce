95d321f0fa0a58608ea94f958a55aa2a07dfc87d
Farhad Ahmad <farhadahmadhassanzai@gmail.com>
2026-04-05 22:36:56 +0500

    Created Product Upload Feature

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  price: {price: Number, required: true},
-  image: {price: Array, required: true},
-  category: {price: String, required: true},
-  subCategory: {price: String, required: true},
-  sizes: {price: Array, required: true},
-  bestSeller: {price: Boolean},
-  date: {price: Number, required: true},
+  price: {type: Number, required: true},
+  image: {type: Array, required: true},
+  category: {type: String, required: true},
+  subCategory: {type: String, required: true},
+  sizes: {type: Array, required: true},
+  bestSeller: {type: Boolean},
+  date: {type: Number, required: true},
 });
 
 const productModel = mongoose.models.product || mongoose.model("product", productSchema)
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDb from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 
 // App config
@@ -18,9 +19,13 @@ app.use(cors())
 
 // Api endpoints
 app.use('/api/user', userRouter)
+app.use('/api/product',productRouter)
+
 app.get('/', (req, res)=>{
     res.send('API working')
 })
+
+
 
 app.listen(port, ()=>{
     console.log('Server Started on Port: ' + port)
